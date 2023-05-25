de2196995973b1220428a684fa4abe3d3a02bfe7
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-05-25 13:23:11 +0900

    showcode fin

--- src/components/showCode.tsx
+++ src/components/showCode.tsx
@@ -3,43 +3,34 @@
 import ReactDOMServer from "react-dom/server";
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
-
-const repeatCharacter = (count: number, character: string = "\t") => {
-	return character.repeat(count);
-};
-
-const nodeToString: (node: React.ReactNode, recallNum?: number) => string = (node, recallNum = 0) => {
-	console.log(recallNum);
-	if (typeof node === "string" || typeof node === "number") {
-		return String(node);
-	}
-
-	if (Array.isArray(node)) {
-		return node.map(nodeToString).join("");
-	}
-
-	if (React.isValidElement(node)) {
-		const { type, props } = node;
-		const children = props.children || "";
-		console.log(type, recallNum);
-		return `${repeatCharacter(recallNum)}<${type}>\n${nodeToString(children, recallNum + 1)}\n</${type}>`;
-	} else {
-		return "";
-	}
-};
+import { monoBlue } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import parse from "html-react-parser";
 
 type Props = {
 	code: string;
 };
 
 const ShowCode = ({ code }: Props) => {
-	const CodeString = nodeToString(code);
-	const codeString = "console.log('hello')";
+	const hlmlElem = parse(code);
 	return (
-		<SyntaxHighlighter language='javascript' style={dark}>
-			{code}
-		</SyntaxHighlighter>
+		<div
+			style={{
+				display: "grid",
+				gridTemplateColumns: "1fr 1fr",
+				gap: "1rem",
+				width: "100%",
+				border: "solid 1px",
+				padding: "1rem",
+				borderRadius: "0.5rem",
+			}}
+		>
+			<div style={{ gridColumn: 1 }}>
+				<SyntaxHighlighter language='htmlbars' style={monoBlue}>
+					{code}
+				</SyntaxHighlighter>
+			</div>
+			<div style={{ gridColumn: 2 }}>{hlmlElem}</div>
+		</div>
 	);
 };
 

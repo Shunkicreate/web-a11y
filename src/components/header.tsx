9f64ccae785e27dc0cc149cba29aa0fe947cf57b
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-06-02 18:51:22 +0900

    headerアクセシビリティ改善

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ const Header = () => {
     return (
         <header style={{ minHeight: "4rem", borderBottom: "1px solid #999", borderColor: "rgba(0, 0, 0)", display:"flex",  justifyContent: "space-between"
     }}>
-            <div style={{ width:"fit-content", height:"fit-content", margin:"auto 4rem" }}>
-                Webアクセシビリティ勉強会
+            <div style={{ margin:"auto 4rem", backgroundColor: "0000" }}>
+                <Link href={"/"}>Webアクセシビリティ勉強会</Link>
             </div>
             <nav style={{ margin:"auto 4rem" }}>
                 <Link href={"/"}>Top</Link>

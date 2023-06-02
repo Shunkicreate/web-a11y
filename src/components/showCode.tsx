ae8b1ae66a6bfd506214aeb14ce2ba16872bc330
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-06-03 01:58:05 +0900

    oyasu-mi

--- src/components/showCode.tsx
+++ src/components/showCode.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { monoBlue } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import parse from "html-react-parser";
+import style from "./showCode.module.css";
 
 type Props = {
 	code: string;
@@ -14,25 +15,16 @@ const ShowCode = ({ code }: Props) => {
 	const hlmlElem = parse(code);
 	return (
 		<div
-			style={{
-				display: "grid",
-				gridTemplateColumns: "1fr 1fr",
-				gap: "1rem",
-				width: "100%",
-				border: "solid 1px",
-				padding: "1rem",
-				borderRadius: "0.5rem",
-			}}
+			className={style.showCode}
 		>
-			<div style={{ gridColumn: 1 }}>
+			<div className={style.showRow}>
 				<SyntaxHighlighter language='htmlbars' style={monoBlue}>
 					{code}
 				</SyntaxHighlighter>
 			</div>
-			<div style={{ gridColumn: 2 }}>{hlmlElem}</div>
+			<div className={style.showHTML}>{hlmlElem}</div>
 		</div>
 	);
 };
 
 export default ShowCode;
-

8b6c7134860745c2ce477170cc89c8d5654dc1ec
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-06-22 20:04:17 +0900

    とりあえず終わらす

--- src/app/3-form/2-input-assistance/page.tsx
+++ src/app/3-form/2-input-assistance/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import ShowCode from "@/components/showCode";
+import { renderToString } from "react-dom/server";
 const Page = () => {
 	const OneFormControl = `
 <form action="">
@@ -37,7 +39,7 @@ const Page = () => {
 		</label>
 	</div>
 </form>
-	`
+	`;
 	const GoodAutoFormInput = `
 <form>
 	<div>
@@ -161,13 +163,13 @@ const Page = () => {
 		</label>
 	</div>
 </form>
-	`
+	`;
 	const BadFormControl = `
 	<label>
 		商品の色（青・緑・黄・赤のいずれかを入力してください）
 		<input type="text" />
 	</label>
-`
+`;
 	const FormType = `
 		<form>
 			<div>
@@ -266,7 +268,122 @@ const Page = () => {
 				</label>
 			</div>
 		</form>
+	`;
+
+	const inputType = `
+		<div>
+			<form>
+				<div>
+					<label htmlFor='inputTypeDecimal'>decimal</label>
+					<br />
+					<input type='text' id='inputTypeDecimal' />
+				</div>
+				<div>
+					<label htmlFor='inputTypeEmail'>email</label>
+					<br />
+					<input type='email' id='inputTypeEmail' />
+				</div>
+				<div>
+					<label htmlFor='inputTypeNumeric'>numeric</label>
+					<br />
+					<input type='text' id='inputTypeNumeric' />
+				</div>
+				<div>
+					<label htmlFor='inputTypeSearch'>search</label>
+					<br />
+					<input type='search' id='inputTypeSearch' />
+				</div>
+				<div>
+					<label htmlFor='inputTypeTel'>tel</label>
+					<br />
+					<input type='tel' id='inputTypeTel' />
+				</div>
+				<div>
+					<label htmlFor='inputTypeUrl'>url</label>
+					<br />
+					<input type='url' id='inputTypeUrl' />
+				</div>
+			</form>
+		</div>
+	`;
+
+	const inputMode = `
+		<div>
+			<form>
+				<div>
+					<label htmlFor='inputModeDecimal'>decimal</label>
+					<br />
+					<input type='text' id='inputModeDecimal' inputMode='decimal' />
+				</div>
+				<div>
+					<label htmlFor='inputModeEmail'>email</label>
+					<br />
+					<input type='email' id='inputModeEmail' inputMode='email' />
+				</div>
+				<div>
+					<label htmlFor='inputModeNumeric'>numeric</label>
+					<br />
+					<input type='text' id='inputModeNumeric' inputMode='numeric' />
+				</div>
+				<div>
+					<label htmlFor='inputModeSearch'>search</label>
+					<br />
+					<input type='search' id='inputModeSearch' inputMode='search' />
+				</div>
+				<div>
+					<label htmlFor='inputModeTel'>tel</label>
+					<br />
+					<input type='tel' id='inputModeTel' inputMode='tel' />
+				</div>
+				<div>
+					<label htmlFor='inputModeUrl'>url</label>
+					<br />
+					<input type='url' id='inputModeUrl' inputMode='url' />
+				</div>
+			</form>
+		</div>
+	`;
+
+	const limitInput = `
+		<div>
+			<input type="range" min="10" max="100" step="10">
+		</div>
 	`
+
+	const listBox = `
+		<div>
+			<label>
+				都道府県
+				<select>
+					<option value="" selected>未選択</option>
+					<option value="北海道">北海道</option>
+					<option value="青県森">青森県</option>
+					<option value="秋田県">秋田県</option>
+					<option value="山形県">山形県</option>
+					<option value="岩手県">岩手県</option>
+					<option value="福島県">福島県</option>
+				</select>
+			</label>
+		</div>
+	`
+
+	const comboBox = `
+		<div>
+			<label>
+				都道府県
+				<input type="text" list="prefecture-list" />
+				<datalist id="prefecture-list">
+					<option value="北海道">北海道</option>
+					<option value="青県森">青森県</option>
+					<option value="秋田県">秋田県</option>
+					<option value="山形県">山形県</option>
+					<option value="岩手県">岩手県</option>
+					<option value="福島県">福島県</option>
+				</datalist>
+			</label>
+		</div>
+	`
+
 	return (
 		<div>
 			<h1>入力の支援</h1>
@@ -278,18 +395,13 @@ const Page = () => {
 			</ul>
 			<p>それでは具体例とともに改善方法を見ていく。</p>
 			<h2>必要以上に入力を求めている</h2>
-			<p>
-				ユーザに対して必要以上に入力を求めている場合がある。
-			</p>
+			<p>ユーザに対して必要以上に入力を求めている場合がある。</p>
 			<ul>
 				<li>一つのフォームに対してフォームコントロールが多すぎる</li>
 				<li>システム上重要でないフォームコントロール</li>
 				<li>直ちに入力が必要でない項目</li>
 			</ul>
-			<p>
-				入力に時間が多くかかるユーザにとってこれらのフォームは深刻な問題である。
-
-			</p>
+			<p>入力に時間が多くかかるユーザにとってこれらのフォームは深刻な問題である。</p>
 			<h3>改善策 -入力項目を最小限に-</h3>
 			<p>
 				まず、入力が必要かどうか検討すべき。不必要な項目は削除すべきであり、後から入力するので良ければ後回しにすべき。
@@ -297,9 +409,7 @@ const Page = () => {
 				また、必須項目の内容も検討し、出来るだけ任意項目にするべきである。そうすることでユーザ（特に入力に時間のかかる方）が大きな恩恵を受けられる。
 			</p>
 			<h2>一つの入力値を表すフォームコントロールが分割されている</h2>
-			<p>
-				一つの入力値を表すフォームコントロールが分割されている場合がある。
-			</p>
+			<p>一つの入力値を表すフォームコントロールが分割されている場合がある。</p>
 			<ul>
 				<li>姓と名</li>
 				<li>メールアドレスの@の前後</li>
@@ -310,57 +420,53 @@ const Page = () => {
 				これらのような事例があると、様々な場面で障害が発生する。マシンリーダビリティ的視点ではブラウザによる自動補完が効きにくくなる。autoComplete属性が正常に働かないため、自動補完が効きにくい。また、ユーザ視点では、コピー&ペーストをすることが難しくなる。また、ミドルネームを入力するときに姓と名のどちらに含めるべきか分からない。
 			</p>
 			<h3>改善策 -一つの入力値を表すフォームコントロールをまとめる-</h3>
-			<p>
-				以下の入力値を表すフォームコントロールは一つにまとめて、autoComplete属性をつける。
-			</p>
+			<p>以下の入力値を表すフォームコントロールは一つにまとめて、autoComplete属性をつける。</p>
 			<ul>
 				<li>名前</li>
 				<li>メールアドレス</li>
 				<li>電話番号</li>
 				<li>クレジットカード番号</li>
 			</ul>
-			<p>
-				下に実装例を示す
-			</p>
+			<p>下に実装例を示す</p>
 			<ShowCode code={OneFormControl}></ShowCode>
 			<h2>ブラウザによる自動補完が使えない</h2>
 			<p>
 				多くのブラウザはユーザが過去に入力した情報を自動補完する。さらに、周辺の入力欄に対しても自動で入力してくれる。先に自動補完が機能しない悪い例を示す。
 			</p>
 			<h3>悪い例: 自動補完が機能しないフォーム</h3>
-			<p>
-				下に悪い実装例を示す
-			</p>
+			<p>下に悪い実装例を示す</p>
 			<ShowCode code={BadAutoFormInput}></ShowCode>
-			<p>
-				入力値が自動補完されないと以下のような負荷がかかる。
-			</p>
+			<p>入力値が自動補完されないと以下のような負荷がかかる。</p>
 			<ul>
 				<li>ユーザが入力値を思い出す負荷</li>
 				<li>手入力する負荷</li>
 			</ul>
 			<h3>改善策 -自動補完できるようにマークアップする-</h3>
-			<p>
-				先ほど同様にautoComplete属性を付ける。ただし、以下の2点に気を付ける。
-			</p>
+			<p>先ほど同様にautoComplete属性を付ける。ただし、以下の2点に気を付ける。</p>
 			<ul>
 				<li>フォームコントロールをform要素で囲う</li>
 				<li>autoComplete属性とともにname属性にも適切な値を設定する必要がある</li>
 			</ul>
 			<ShowCode code={GoodAutoFormInput}></ShowCode>
 			<h2>入力値の種類に対して適切な入力タイプが選択されていない</h2>
-			<p>フォームコントロールには入力値の種類によって入力タイプに応じたUIを表示できる。入力するときに本来許可されない種類の入力値が入力出来たり、数値の入力が行いやすくなるため、適切な入力タイプを選ぶ必要がある。</p>
-			<h3>改善策 -入力値の種類に応じてタイプを指定する-</h3>
 			<p>
-				入力値の種類に応じてタイプを指定することで入力のしやすさを改善できる．以下がタイプ指定の例である．
+				フォームコントロールには入力値の種類によって入力タイプに応じたUIを表示できる。入力するときに本来許可されない種類の入力値が入力出来たり、数値の入力が行いやすくなるため、適切な入力タイプを選ぶ必要がある。
 			</p>
+			<h3>改善策 -入力値の種類に応じてタイプを指定する-</h3>
+			<p>入力値の種類に応じてタイプを指定することで入力のしやすさを改善できる．以下がタイプ指定の例である．</p>
 			<ShowCode code={FormType}></ShowCode>
-
-
+			<p>ここからはまにあいませんでした。。。。。。</p>
+			<ShowCode code={inputType}></ShowCode>
+			<ShowCode code={inputMode}></ShowCode>
+			<ShowCode code={limitInput}></ShowCode>
 			<h2>選択肢のある入力値に対して適切なフォームコントロールが選択されていない</h2>
-			<p>入力値が限定された値のうちの一つであるにもかかわらず，自由入力形式のフォームコントロールが使われている場合がある．例えば以下のような例がある．</p>
+			<p>
+				入力値が限定された値のうちの一つであるにもかかわらず，自由入力形式のフォームコントロールが使われている場合がある．例えば以下のような例がある．
+			</p>
 			<ShowCode code={BadFormControl}></ShowCode>
 			上記の例のように値を自由に入力出来てしまうと想定された選択肢以外の値を入力出来たり，入力ミスが起きたりする．視線の動きや頭部の動きによって操作を行う運動障害のあるユーザにとっては手入力が大きなコストになる．障害のあるユーザを含めて多くのユーザが正しい選択肢を選びやすくなるように適切なフォームコントロールを選ぶ必要がある．
+			<ShowCode code={listBox}></ShowCode>
+			<ShowCode code={comboBox}></ShowCode>
 		</div>
 	);
 };

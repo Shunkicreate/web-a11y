2a8ba190b14cba8885bd4c770fb4fa15632391bf
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-07-13 03:08:06 +0900

    fix a

--- src/app/4-ui/2-text-size/zoom-contents-size/page.tsx
+++ src/app/4-ui/2-text-size/zoom-contents-size/page.tsx
@@ -5,17 +5,17 @@ const Page = () => {
 	const [flag, setFlag] = useState(false);
 	return (
 		<>
-			<div style={{ width: "100%", backgroundColor: "#f6bd60", color: "black" }}>
+			<div style={{ width: "100%", backgroundColor: "#f6bd60bb", color: "black" }}>
 				<h1>ヘッダー</h1>
 			</div>
 			<div style={{ width: "100%", display: "flex", color: "black" }}>
-				<div style={{ width: "50%", backgroundColor: "#ff99c8" }}>
+				<div style={{ width: "50%", backgroundColor: "#ff99c8bb" }}>
 					<h2>contains1</h2>
 					<p>
 						これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。これはcontains1です。
 					</p>
 				</div>
-				<div style={{ width: "50%", backgroundColor: "#fcf6bd" }}>
+				<div style={{ width: "50%", backgroundColor: "#fcf6bdbb" }}>
 					<h2>contains2</h2>
 					<p>
 						これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。これはcontains2です。
@@ -23,19 +23,19 @@ const Page = () => {
 				</div>
 			</div>
 			<div style={{ width: "100%", display: "flex", color: "black" }}>
-				<div style={{ width: "33.3%", backgroundColor: "#d0f4de" }}>
+				<div style={{ width: "33.3%", backgroundColor: "#d0f4debb" }}>
 					<h2>contains3</h2>
 					<p>
 						これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。これはcontains3です。
 					</p>
 				</div>
-				<div style={{ width: "33.3%", backgroundColor: "#a9def9" }}>
+				<div style={{ width: "33.3%", backgroundColor: "#a9def9bb" }}>
 					<h2>contains4</h2>
 					<p>
 						これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。これはcontains4です。
 					</p>
 				</div>
-				<div style={{ width: "33.3%", backgroundColor: "#e4c1f9" }}>
+				<div style={{ width: "33.3%", backgroundColor: "#e4c1f9bb" }}>
 					<h2>contains5</h2>
 					<p>
 						これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。これはcontains5です。

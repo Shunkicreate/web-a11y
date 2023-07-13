691c19abc49e148a3afbcde5ff1eb35877d45cb1
YukaChoco <sugar.ore.yk@gmail.com>
2023-07-13 23:37:51 +0900

    OGP title,description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
 	title: "Webアクセシビリティ勉強会",
 	description: "このサイトは「Webアプリケーションアクセシビリティ 今日から始める現場からの改善」のアウトプット用サイトです。",
+  openGraph: {
+    title: 'Webアクセシビリティ勉強会',
+    description: 'このサイトは「Webアプリケーションアクセシビリティ 今日から始める現場からの改善」のアウトプット用サイトです。',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

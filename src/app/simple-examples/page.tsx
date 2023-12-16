48e905f8c5e244305ffb6b2f613e5682fb82d419
YukaChoco <sugar.ore.yk@gmail.com>
2023-12-17 03:56:09 +0900

    fontsizeのサンプル追加

---
+++ src/app/simple-examples/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Index = () => {
+	return (
+		<>
+			<h1>アクセシビリティを実感してみる</h1>
+			<p>これは、<Link href="">今日から始めるアクセシビリティ~少しの気遣いでより使いやすいアプリへ~</Link>の記事と対応したシンプルなアクセシビリティお試しサンプルです。実際に触ってみて、アクセシビリティの面白さを実感してみてください。</p>
+			<ul>
+				<li><Link href="simple-examples/font-size">文字サイズの変更と見た目</Link></li>
+			</ul>
+		</>
+	);
+};
+
+export default Index;

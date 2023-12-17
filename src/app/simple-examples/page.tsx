7742714b3f54813f35cb8131201d889f40265771
YukaChoco <sugar.ore.yk@gmail.com>
2023-12-17 23:00:47 +0900

    buttonタグの実装

--- src/app/simple-examples/page.tsx
+++ src/app/simple-examples/page.tsx
@@ -7,6 +7,7 @@ const Index = () => {
 			<p>これは、<Link href="">今日から始めるアクセシビリティ~少しの気遣いでより使いやすいアプリへ~</Link>の記事と対応したシンプルなアクセシビリティお試しサンプルです。実際に触ってみて、アクセシビリティの面白さを実感してみてください。</p>
 			<ul>
 				<li><Link href="simple-examples/font-size">文字サイズの変更と見た目</Link></li>
+				<li><Link href="simple-examples/button">buttonタグによるボタンの実装</Link></li>
 			</ul>
 		</>
 	);

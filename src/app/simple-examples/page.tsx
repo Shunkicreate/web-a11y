0906e0770272f1bf3d00abe1f04b1812820bcf5b
YukaChoco <sugar.ore.yk@gmail.com>
2023-12-18 12:14:10 +0900

    サンプルのページ名変更

--- src/app/simple-examples/page.tsx
+++ src/app/simple-examples/page.tsx
@@ -6,9 +6,9 @@ const Index = () => {
 			<h1>アクセシビリティを実感してみる</h1>
 			<p>これは、<Link href="">今日から始めるアクセシビリティ~少しの気遣いでより使いやすいアプリへ~</Link>の記事と対応したシンプルなアクセシビリティお試しサンプルです。実際に触ってみて、アクセシビリティの面白さを実感してみてください。</p>
 			<ul>
-				<li><Link href="simple-examples/font-size">文字サイズの変更と見た目</Link></li>
-				<li><Link href="simple-examples/button">buttonタグによるボタンの実装</Link></li>
-				<li><Link href="simple-examples/a">aタグによるリンクの実装</Link></li>
+				<li><Link href="simple-examples/1-font-size">文字サイズの変更と見た目</Link></li>
+				<li><Link href="simple-examples/2-button">buttonタグによるボタンの実装</Link></li>
+				<li><Link href="simple-examples/3-a">aタグによるリンクの実装</Link></li>
 			</ul>
 		</>
 	);
